d66f363f2f75e61f18ac8f6bd0d7e2f5f9b83689
OM3X4 <om3x4e@gmail.com>
2024-12-20 09:07:34 +0200

    instruction.html

--- Main.js
+++ Main.js
@@ -1,8 +1,19 @@
-import jwt_decode from "jwt-decode"
 
 
 console.log("hi")
 
+function parseJwt(token) {
+    const base64Url = token.split('.')[1]; // Get the payload part
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Fix encoding
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
 function handleCredentialResponse(googleUser){
-    console.log(jwt_decode(googleUser.credential))
+    // console.log((googleUser.credential))
+    const cred = googleUser.credential
+    console.log(parseJwt(cred))
 }
